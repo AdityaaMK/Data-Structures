ec840743b00912f16b0399899b934333d44c415d
AdityaaMK <44482076+AdityaaMK@users.noreply.github.com>
2020-11-18 22:09:07 -0500

    almost done

--- SuperList/SuperList.java
+++ SuperList/SuperList.java
@@ -1,14 +1,13 @@
-public class SuperList<E extends Comparable<E>> implements Iterable<E> {
+public class SuperList<E> {
     private ListNode root;
     private ListNode end;
-    private int currentIndex;
     private int size;
 
     public SuperList() {
 
     }
 
-    public SuperList(E value) {
+    public SuperList(E root) {
         this.root = new ListNode(root);
         this.end = this.root;
         size = 1;
@@ -24,6 +23,133 @@ public class SuperList<E extends Comparable<E>> implements Iterable<E> {
             newNode.setPrevious(end);
             end = newNode;
         }
+        size++;
+    }
+
+    public void push(E nextValue) {
+        ListNode newNode = new ListNode(nextValue);
+        if (root == null) {
+            root = newNode;
+            end = newNode;
+        } else {
+            end.setNext(newNode);
+            newNode.setPrevious(end);
+            end = newNode;
+        }
+        size++;
+    }
+
+    public void add(int index, E newValue) {
+        if (index > size || index < 0)
+            throw new IndexOutOfBoundsException("Index " + index + " is not in SuperList of size " + size);
+        if (index == 0) {
+            ListNode newNode = new ListNode(newValue);
+            if (root == null) {
+                root = newNode;
+                end = newNode;
+            } else {
+                root.setPrevious(newNode);
+                newNode.setNext(root);
+                root = newNode;
+            }
+        } else if (index == size) {
+            add(newValue);
+            return;
+        } else {
+            ListNode temp = root;
+            for (int i = 0; i < index - 1; i++)
+                temp = temp.getNext();
+            ListNode newNode = new ListNode(newValue);
+            newNode.setNext(temp.getNext());
+            newNode.setPrevious(temp);
+            temp.setNext(newNode);
+            temp.getNext().setPrevious(newNode);
+        }
+        size++;
+    }
+
+    public E remove(int index) {
+        if (index >= size || index < 0)
+            throw new IndexOutOfBoundsException("Index " + index + " is not in SuperList of size " + size);
+        if (index == 0) {
+            E temp = root.getValue();
+            root = root.getNext();
+            if (root != null)
+                root.setPrevious(null);
+            size--;
+            return temp;
+        }
+        if (index == size - 1) {
+            E temp = end.getValue();
+            end = end.getPrevious();
+            if (end != null)
+                end.setNext(null);
+            size--;
+            return temp;
+        }
+        ListNode temp = root; // temp is value we are removing
+        for (int i = 0; i < index - 1; i++)
+            temp = temp.getNext();
+        ListNode prev = temp;
+        temp = temp.getNext();
+        ListNode next = temp.getNext();
+        prev.setNext(next);
+        next.setPrevious(prev);
+        size--;
+        return temp.getValue();
+    }
+
+    public E get(int index) {
+        if (index >= size || index < 0)
+            throw new IndexOutOfBoundsException("Index " + index + " is not in SuperList of size " + size);
+        ListNode temp = root;
+        for (int i = 0; i < index; i++)
+            temp = temp.getNext();
+        return temp.getValue();
+    }
+
+    public E pop() {
+        return remove(size - 1);
+    }
+
+    public E poll() {
+        return remove(0);
+    }
+
+    public E peekStack() {
+        return end != null ? end.getValue() : null;
+    }
+
+    public E peekQueue() {
+        return root != null ? root.getValue() : null;
+    }
+
+    public String toString() {
+        String s = "[";
+        for (ListNode node = root; node != null; node = node.getNext())
+            s += node.getValue() + ", ";
+        return (s.length() > 1 ? s.substring(0, s.length() - 2) : s) + "]";
+    }
+
+    public boolean contains(E value) {
+        for (ListNode node = root; node != null; node = node.getNext())
+            if (value.equals(node.getValue()))
+                return true;
+        return false;
+    }
+
+    public void clear() {
+        root = null;
+        end = null;
+        size = 0;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
     }
 
     public class ListNode {
