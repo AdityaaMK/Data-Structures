d2da8a3135372a66b9e0cad65fbe60e7510d4e16
AdityaaMK <44482076+AdityaaMK@users.noreply.github.com>
2020-11-23 19:14:14 -0500

    finished superlist

--- SuperList/SuperList.java
+++ SuperList/SuperList.java
@@ -1,6 +1,8 @@
+import java.util.EmptyStackException;
+
 public class SuperList<E> {
-    private ListNode root;
-    private ListNode end;
+    private ListNode<E> root;
+    private ListNode<E> end;
     private int size;
 
     public SuperList() {
@@ -8,13 +10,13 @@ public class SuperList<E> {
     }
 
     public SuperList(E root) {
-        this.root = new ListNode(root);
+        this.root = new ListNode<>(root);
         this.end = this.root;
         size = 1;
     }
 
     public void add(E nextValue) {
-        ListNode newNode = new ListNode(nextValue);
+        ListNode<E> newNode = new ListNode<>(nextValue);
         if (root == null) {
             root = newNode;
             end = newNode;
@@ -27,23 +29,14 @@ public class SuperList<E> {
     }
 
     public void push(E nextValue) {
-        ListNode newNode = new ListNode(nextValue);
-        if (root == null) {
-            root = newNode;
-            end = newNode;
-        } else {
-            end.setNext(newNode);
-            newNode.setPrevious(end);
-            end = newNode;
-        }
-        size++;
+        add(nextValue);
     }
 
     public void add(int index, E newValue) {
         if (index > size || index < 0)
             throw new IndexOutOfBoundsException("Index " + index + " is not in SuperList of size " + size);
         if (index == 0) {
-            ListNode newNode = new ListNode(newValue);
+            ListNode<E> newNode = new ListNode<>(newValue);
             if (root == null) {
                 root = newNode;
                 end = newNode;
@@ -56,14 +49,15 @@ public class SuperList<E> {
             add(newValue);
             return;
         } else {
-            ListNode temp = root;
+            ListNode<E> temp = root;
             for (int i = 0; i < index - 1; i++)
                 temp = temp.getNext();
-            ListNode newNode = new ListNode(newValue);
-            newNode.setNext(temp.getNext());
+            ListNode<E> next = temp.getNext();
+            ListNode<E> newNode = new ListNode<>(newValue);
+            newNode.setNext(next);
             newNode.setPrevious(temp);
             temp.setNext(newNode);
-            temp.getNext().setPrevious(newNode);
+            next.setPrevious(newNode);
         }
         size++;
     }
@@ -72,27 +66,18 @@ public class SuperList<E> {
         if (index >= size || index < 0)
             throw new IndexOutOfBoundsException("Index " + index + " is not in SuperList of size " + size);
         if (index == 0) {
-            E temp = root.getValue();
-            root = root.getNext();
-            if (root != null)
-                root.setPrevious(null);
-            size--;
-            return temp;
+            return poll();
+
         }
         if (index == size - 1) {
-            E temp = end.getValue();
-            end = end.getPrevious();
-            if (end != null)
-                end.setNext(null);
-            size--;
-            return temp;
+            return pop();
         }
-        ListNode temp = root; // temp is value we are removing
+        ListNode<E> temp = root; // temp is value we are removing
         for (int i = 0; i < index - 1; i++)
             temp = temp.getNext();
-        ListNode prev = temp;
+        ListNode<E> prev = temp;
         temp = temp.getNext();
-        ListNode next = temp.getNext();
+        ListNode<E> next = temp.getNext();
         prev.setNext(next);
         next.setPrevious(prev);
         size--;
@@ -102,22 +87,46 @@ public class SuperList<E> {
     public E get(int index) {
         if (index >= size || index < 0)
             throw new IndexOutOfBoundsException("Index " + index + " is not in SuperList of size " + size);
-        ListNode temp = root;
+        ListNode<E> temp = root;
         for (int i = 0; i < index; i++)
             temp = temp.getNext();
         return temp.getValue();
     }
 
     public E pop() {
-        return remove(size - 1);
+        if (size == 0)
+            throw new EmptyStackException();
+        if (size == 1) {
+            E val = end.getValue();
+            clear();
+            return val;
+        }
+        E val = end.getValue();
+        end = end.getPrevious();
+        end.setNext(null);
+        size--;
+        return val;
     }
 
     public E poll() {
-        return remove(0);
+        if (size == 0)
+            throw new EmptyStackException();
+        if (size == 1) {
+            E val = end.getValue();
+            clear();
+            return val;
+        }
+        E val = root.getValue();
+        root = root.getNext();
+        root.setPrevious(null);
+        size--;
+        return val;
     }
 
     public E peekStack() {
-        return end != null ? end.getValue() : null;
+        if (end == null)
+            throw new EmptyStackException();
+        return end.getValue();
     }
 
     public E peekQueue() {
@@ -126,13 +135,13 @@ public class SuperList<E> {
 
     public String toString() {
         String s = "[";
-        for (ListNode node = root; node != null; node = node.getNext())
+        for (ListNode<E> node = root; node != null; node = node.getNext())
             s += node.getValue() + ", ";
         return (s.length() > 1 ? s.substring(0, s.length() - 2) : s) + "]";
     }
 
     public boolean contains(E value) {
-        for (ListNode node = root; node != null; node = node.getNext())
+        for (ListNode<E> node = root; node != null; node = node.getNext())
             if (value.equals(node.getValue()))
                 return true;
         return false;
@@ -152,11 +161,26 @@ public class SuperList<E> {
         return size;
     }
 
-    public class ListNode {
+    // public SuperList<E> copy() {
+    // SuperList<E> duplicate = new SuperList<>();
+    // return duplicate.addAll(this);
+    // }
+
+    // public SuperList<E> addAll(SuperList<E> other) {
+    // for (E a : other)
+    // add(a);
+    // return this;
+    // }
+
+    public static interface ListFilter<E> {
+        public boolean filter(E obj);
+    }
+
+    public class ListNode<E> {
 
         private E value;
-        private ListNode next;
-        private ListNode previous;
+        private ListNode<E> next;
+        private ListNode<E> previous;
 
         public ListNode(E value) {
             this.value = value;
@@ -170,11 +194,11 @@ public class SuperList<E> {
             this.value = value;
         }
 
-        public ListNode getPrevious() {
+        public ListNode<E> getPrevious() {
             return previous;
         }
 
-        public void setPrevious(ListNode previous) {
+        public void setPrevious(ListNode<E> previous) {
             this.previous = previous;
         }
 
@@ -182,11 +206,11 @@ public class SuperList<E> {
             return previous != null;
         }
 
-        public ListNode getNext() {
+        public ListNode<E> getNext() {
             return next;
         }
 
-        public void setNext(ListNode next) {
+        public void setNext(ListNode<E> next) {
             this.next = next;
         }
 
--- SuperList/SuperListRunner.java
+++ SuperList/SuperListRunner.java
@@ -3,88 +3,123 @@ import java.util.*;
 public class SuperListRunner {
 
 	public SuperListRunner() {
-		// This is to test your code. If it works, you should get my answers!
-		Queue<Integer> stack = new LinkedList<Integer>();
-		System.out.println(stack.poll());
-		SuperList<Integer> list = new SuperList<Integer>();
-		list.add(10);
-		list.add(0, 5);
-		list.add(list.size() - 1, 8);
-		list.add(1, 12);
-		list.add(20);
-		list.add(25);
-		list.add(4, 100);
-		list.add(0, 0);
-		list.add(list.size() - 1, 1000); // Should put 1000 in front of 25 (which had been the end)
-		list.add(list.size(), 555); // should make 555 the last number in the list (the end)
-		System.out.println("Size: " + list.size());
-		System.out.println("List: " + list);
-
-		System.out.println("Get tests: ");
-		System.out.println("index 0: " + list.get(0));
-		System.out.println("index 3: " + list.get(3));
-		System.out.println("Peek (queue): " + list.peekQueue());
-		System.out.println("Peek (stack): " + list.peekStack());
-
-		System.out.println("\nRemove tests: ");
-		System.out.println("Remove index 2: " + list.remove(2));
-		System.out.println("Remove index 3: " + list.remove(3));
-		System.out.println("Remove index 0: " + list.remove(0));
-		System.out.println("Remove index list.size()-1: " + list.remove(list.size() - 1));
-
-		System.out.println("\nRemove values at indexes 2, 3, 0, and then size()-1 respectively.");
-		System.out.println("\tList: " + list);
-
-		System.out.println("Pop and Poll tests: ");
-		System.out.println("Popped: " + list.pop());
-		System.out.println("Polled: " + list.poll());
-		System.out.println("Pop a value and then poll a value.");
-		System.out.println("\tList: " + list);
-
-		System.out.println("\nContains Test:");
-		System.out.println("\tContains 4: " + list.contains(4));
-		System.out.println("\tContains 20: " + list.contains(20));
-
-		System.out.println("\nHere is the list, \"unstacked\": ");
-		SuperList<Integer> tempList = new SuperList<Integer>();
-		String output = "[";
-		while (!list.isEmpty()) {
-			int num = list.pop();
-			output += num;
-			if (list.peekStack() != null)
-				output += ", ";
-			tempList.add(num);
-
+		SuperList<Integer> arrayList = new SuperList<>();
+		for (int i = 0; i < 30; i++) {
+			int rand = (int) (Math.random() * 1000) + 1;
+			arrayList.add(rand);
 		}
-		output += "]";
-		list = tempList;
-		System.out.println("Stack: " + output);
+		System.out.println("List: " + arrayList);
+		System.out.println("Size: " + arrayList.size());
 
-		System.out.println("\nnHere is the list, \"unqueued\": ");
-		SuperList<Integer> tempListQ = new SuperList<Integer>();
-		output = "[";
-		while (!list.isEmpty()) {
-			int num = list.poll();
-			output += num;
-			if (list.peekQueue() != null)
-				output += ", ";
-			tempListQ.push(num);
-
+		SuperList<Integer> stackList = new SuperList<>();
+		while (!arrayList.isEmpty()) {
+			int temp = arrayList.remove(arrayList.size() - 1);
+			stackList.push(temp);
 		}
-		output += "]";
-		list = tempListQ;
-		System.out.println("Queue: " + output);
+		SuperList<Integer> queueList = new SuperList<>();
+		System.out.print("Stack List: ");
+		while (!stackList.isEmpty()) {
+			int temp = stackList.pop();
+			System.out.print(temp + " ");
+			queueList.add(temp);
+		}
+		System.out.print("\nQueue List: ");
+		while (!queueList.isEmpty()) {
+			int temp = queueList.poll();
+			System.out.print(temp + " ");
+			arrayList.add((int) (Math.random() * arrayList.size()), temp);
+		}
+		int sum = 0;
+		int evenSum = 0;
+		int oddSum = 0;
 
-		System.out.println();
-		list.clear();
-		System.out.println("\nClear List!");
-		System.out.println("\tList: " + list);
+		SuperList<Integer> evensList = new SuperList<>();
+		for (int i = 0; i < arrayList.size(); i++) {
+			sum += arrayList.get(i);
+			if (i % 2 == 0) {
+				evenSum += arrayList.get(i);
+			} else
+				oddSum += arrayList.get(i);
+			if (arrayList.get(i) % 2 == 0) {
+				evensList.add(arrayList.get(i));
+			}
+		}
+		for (int i = 0; i < evensList.size(); i++) {
+			arrayList.add(evensList.get(i));
+		}
+		System.out.println("\nSum: " + sum);
+		System.out.println("Even Sum: " + evenSum);
+		System.out.println("Odd Sum: " + oddSum);
+		System.out.println("Duplicated Evens: " + arrayList);
+		for (int i = 0; i < arrayList.size(); i++) {
+			if (arrayList.get(i) % 3 == 0) {
+				arrayList.remove(i);
+			}
+		}
+		System.out.println("List without mod 3 values: " + arrayList);
+		arrayList.add(3, 55555);
+		System.out.println("List with 55555: " + arrayList);
+		for (int i = 0; i < arrayList.size() - 1; i++) {
+			int minIndex = i;
+			for (int j = i + 1; j < arrayList.size(); j++) {
+				if (arrayList.get(j) < arrayList.get(minIndex))
+					minIndex = j;
+			}
+			int temp = arrayList.remove(minIndex);
+			arrayList.add(minIndex, arrayList.get(i));
+			arrayList.remove(i);
+			arrayList.add(i, temp);
+		}
+		System.out.println("Sorted List in Ascending Order: " + arrayList);
 
-		SuperList<String> stringList = new SuperList<String>();
-		stringList.add("St1");
-		stringList.add("St2");
-		stringList.add("St3");
-		System.out.println("String List: " + stringList);
+		double median;
+		if (arrayList.size() % 2 == 1) {
+			median = arrayList.get(arrayList.size() / 2);
+		} else {
+]			median = ((double) arrayList.get(arrayList.size() / 2) + (double) arrayList.get(arrayList.size() / 2 - 1))
+					/ 2;
+		}
+
+		System.out.println("Median: " + median);
+		System.out.print("Before Median: ");
+		for (int i = 0; i < arrayList.size(); i++) {
+			if (arrayList.get(i) < median)
+				System.out.print(arrayList.get(i) + " ");
+		}
+		System.out.print("\nAfter Median: ");
+		for (int i = 0; i < arrayList.size(); i++) {
+			if (arrayList.get(i) > median)
+				System.out.print(arrayList.get(i) + " ");
+		}
+
+		SuperList<String> wordList = new SuperList<>();
+		String sentence = "Hi, my name is Adityaa. How are you doing today? Pleasure to meet you.";
+		String[] arr = sentence.split("\\W+");
+		for (String s : arr)
+			wordList.add(s);
+		System.out.println("\nEntire List: " + wordList);
+		for (int i = 0; i < wordList.size(); i++)
+			if (wordList.get(i).length() <= 3) {
+				wordList.remove(i);
+				i--;
+			}
+		System.out.println("Words longer than 3 chars: " + wordList);
+
+		for (int i = 1; i < wordList.size(); i++) {
+			int key = wordList.get(i).length();
+			int j = i - 1;
+			while (j >= 0 && wordList.get(j).length() > key) {
+				j--;
+			}
+			wordList.add(j + 1, wordList.get(i));
+			wordList.remove(i + 1);
+		}
+		System.out.println("Sorted Word List in Ascending Order: " + wordList);
+
+		double avgLength = 0;
+		for (int i = 0; i < wordList.size(); i++)
+			avgLength += wordList.get(i).length();
+		System.out.println("Average Word Length: " + avgLength / wordList.size());
 	}
 
 	public static void main(String[] args) {
